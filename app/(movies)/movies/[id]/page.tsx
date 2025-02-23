5f0f91987067e8aebbe44b39e92921102f654729
Daeun Kim(김다은) <daeun.kim@dncompany.com>
2025-02-24 00:03:07 +0900

    DEPLOY READY

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -3,20 +3,17 @@ import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
 interface IParams {
-  params: {id: string};
+  params: { id: string };
 }
-export async function generateMetadata({params:{id}} : IParams) {
+
+export async function generateMetadata({ params: { id } }: IParams) {
   const movie = await getMovie(id);
   return {
     title: movie.title,
-  }
+  };
 }
 
-export default async function MovieDetail({
-  params: { id },
-}: {
-  IParams: { id: string };
-}) {
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
